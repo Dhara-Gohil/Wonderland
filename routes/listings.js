a934f229a5902088004a962cecf154484de8f24b
Dhara Gohil <dharagohil3333@gmail.com>
2025-03-24 15:12:45 +0530

    Adding router.route

--- routes/listings.js
+++ routes/listings.js
@@ -6,43 +6,22 @@ const {isLoggedIn,isOwner,validatelisting} = require("../middleware.js");
 
 const listingController = require("../controllers/listings.js");
 
+router
+    .route("/")
+    .get(wrapAsync(listingController.index))
+    .post(isLoggedIn,validatelisting , wrapAsync (listingController.createNewListing))
 
-// Index route: List all listings
-router.get("/",wrapAsync(listingController.index));
 
 // New listing form route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
 
-// Show route: View a specific listing
-router.get("/:id", wrapAsync(listingController.showListings));
-
-// Create listing route
-router.post("/", isLoggedIn,validatelisting , wrapAsync (listingController.createNewListing));
+router
+    .route("/:id")
+    .get(wrapAsync(listingController.showListings))
+    .put(isLoggedIn ,isOwner,validatelisting, wrapAsync (listingController.UpdateListing))
+    .delete(isLoggedIn , isOwner, wrapAsync ( listingController.deleteListing))
 
 // Edit form route
 router.get("/:id/edit", isLoggedIn, isOwner,wrapAsync (listingController.editListing));
-
-// router.get("/:id/edit", async (req, res) => {
-//     const { id } = req.params;
-//     const listing = await Listing.findById(id);
-//     res.render("listings/edit", { listing });
-// });
-
-// Update route
-router.put("/:id", isLoggedIn ,isOwner,validatelisting, wrapAsync (listingController.UpdateListing));
-
-// router.put("/:id", async (req, res) => {
-//     const { id } = req.params;
-//     try {
-//         const updatedListing = await Listing.findByIdAndUpdate(id, req.body.listing, { new: true });
-//         res.redirect(`/listings/${updatedListing._id}`);
-//     } catch (err) {
-//         console.error("Error updating listing:", err);
-//         res.send("Error while updating the listing.");
-//     }
-// });
-
-// Delete route
-router.delete("/:id" , isLoggedIn , isOwner, wrapAsync ( listingController.deleteListing));
 
 module.exports = router; 
