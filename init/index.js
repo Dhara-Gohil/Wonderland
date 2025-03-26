e3f96fe8644190ebe7d5aeebd2a87de83149c094
Dhara Gohil <dharagohil3333@gmail.com>
2025-03-26 23:15:02 +0530

    data added

--- init/index.js
+++ init/index.js
@@ -1,26 +1,60 @@
+// const mongoose = require("mongoose");
+// const initData = require("./data.js");
+// const Listing = require("../models/listing.js");
+
+
+// const mongo_url = "mongodb://127.0.0.1:27017/wonderland";
+
+// async function main() { // connection of mongodb..
+//     await mongoose.connect(mongo_url);
+// }
+
+// main().then(()=>{
+//     console.log("connected to DB !")
+// })
+// .catch((err)=>{
+//     console.log(err);
+// });
+
+// const initDB = async () => { // delete exixting data and insert data from data.js with key data in variable initData.
+//     await Listing.deleteMany({});
+//     initData.data = initData.data.map((obj)=>({...obj,owner:"67cd4bba431149c9fa0f43fc"}))
+//     await Listing.insertMany(initData.data);
+//     console.log("data inserted !");
+// }
+
+// initDB(); // above function call
+
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
+// MongoDB Atlas or Local Connection
+const mongo_url = process.env.ATLASDB_URL || "mongodb://127.0.0.1:27017/wonderland";
 
-const mongo_url = "mongodb://127.0.0.1:27017/wonderland";
-
-async function main() { // connection of mongodb..
-    await mongoose.connect(mongo_url);
+// MongoDB Connection
+async function main() {
+    try {
+        await mongoose.connect(mongo_url);
+        console.log("Connected to MongoDB!");
+    } catch (err) {
+        console.error("MongoDB Connection Error:", err);
+    }
 }
 
-main().then(()=>{
-    console.log("connected to DB !")
-})
-.catch((err)=>{
-    console.log(err);
-});
+main();
 
-const initDB = async () => { // delete exixting data and insert data from data.js with key data in variable initData.
-    await Listing.deleteMany({});
-    initData.data = initData.data.map((obj)=>({...obj,owner:"67cd4bba431149c9fa0f43fc"}))
-    await Listing.insertMany(initData.data);
-    console.log("data inserted !");
-}
+const initDB = async () => {
+    try {
+        await Listing.deleteMany({}); // Delete existing listings
+        initData.data = initData.data.map((obj) => ({ ...obj, owner: "67cd4bba431149c9fa0f43fc" }));
+        await Listing.insertMany(initData.data);
+        console.log("Data inserted successfully!");
+    } catch (err) {
+        console.error("Error inserting data:", err);
+    } finally {
+        mongoose.connection.close(); // Close connection after inserting data
+    }
+};
 
-initDB(); // above function call
+initDB();
